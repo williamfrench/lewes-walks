1ab274f990fa7df04cfe1859f8616311f286108f
William French <idontactuallyneedthislol@gmail.com>
2026-03-12 21:11:55 +0000

    Initial commit

---
+++ walks.js
@@ -0,0 +1,14 @@
+const walks = [
+  { slug: 'one-oclock',    label: "One O'clock \u2013 Isfield" },
+  { slug: 'two-oclock',    label: "Two O'clock \u2013 Halland" },
+  { slug: 'three-oclock',  label: "Three O'clock" },
+  { slug: 'four-oclock',   label: "Four O'clock" },
+  { slug: 'five-oclock',   label: "Five O'clock" },
+  { slug: 'six-oclock',    label: "Six O'clock" },
+  { slug: 'seven-oclock',  label: "Seven O'clock" },
+  { slug: 'eight-oclock',  label: "Eight O'clock \u2013 Brighton" },
+  { slug: 'nine-oclock',   label: "Nine O'clock" },
+  { slug: 'ten-oclock',    label: "Ten O'clock" },
+  { slug: 'eleven-oclock', label: "Eleven O'clock" },
+  { slug: 'twelve-oclock', label: "Twelve O'clock \u2013 Newick" },
+];
