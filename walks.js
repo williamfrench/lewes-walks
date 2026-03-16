9b3a4f73af63b469bc769660550e4e9ed5781adf
William French <idontactuallyneedthislol@gmail.com>
2026-03-16 15:13:24 +0000

    Add lunch stops waypointers

--- walks.js
+++ walks.js
@@ -1,14 +1,21 @@
 const walks = [
-{ slug: 'one-oclock',    'shortlabel': '1 - Isfield',  label: "One O'clock - Isfield" },
-  { slug: 'two-oclock',    'shortlabel': '2 - Halland',  label: "Two O'clock - Halland" },
-  { slug: 'three-oclock',  'shortlabel': '3',  label: "Three O'clock" },
-  { slug: 'four-oclock',   'shortlabel': '4',  label: "Four O'clock" },
-  { slug: 'five-oclock',   'shortlabel': '5',  label: "Five O'clock" },
-  { slug: 'six-oclock',    'shortlabel': '6',  label: "Six O'clock" },
-  { slug: 'seven-oclock',  'shortlabel': '7',  label: "Seven O'clock" },
-  { slug: 'eight-oclock',  'shortlabel': '8 - Brighton',  label: "Eight O'clock - Brighton" },
-  { slug: 'nine-oclock',   'shortlabel': '9 - Devil\'s Dyke',  label: "Nine O'clock - Devil\'s Dyke" },
-  { slug: 'ten-oclock',    'shortlabel': '10', label: "Ten O'clock" },
-  { slug: 'eleven-oclock', 'shortlabel': '11', label: "Eleven O'clock" },
-  { slug: 'twelve-oclock', 'shortlabel': '12 - Newick', label: "Twelve O'clock - Newick" },
+  { slug: 'one-oclock',    'shortlabel': '1 - Isfield',        label: "One O'clock - Isfield",       waypoints: [] },
+  { slug: 'two-oclock',    'shortlabel': '2 - Halland',        label: "Two O'clock - Halland",        waypoints: [] },
+  { slug: 'three-oclock',  'shortlabel': '3',                  label: "Three O'clock",                waypoints: [] },
+  { slug: 'four-oclock',   'shortlabel': '4',                  label: "Four O'clock",                 waypoints: [] },
+  { slug: 'five-oclock',   'shortlabel': '5',                  label: "Five O'clock",                 waypoints: [] },
+  { slug: 'six-oclock',    'shortlabel': '6',                  label: "Six O'clock",                  waypoints: [] },
+  { slug: 'seven-oclock',  'shortlabel': '7',                  label: "Seven O'clock",                waypoints: [] },
+  { slug: 'eight-oclock',  'shortlabel': '8 - Brighton',       label: "Eight O'clock - Brighton",     waypoints: [] },
+  { slug: 'nine-oclock',   'shortlabel': "9 - Devil's Dyke",   label: "Nine O'clock - Devil's Dyke",  waypoints: [
+    { lat: 50.896557, lng: -0.164240, name: "The Plough Inn", icon:'pub'},
+    { lat: 50.904657, lng: -0.146926, name: "The Coffee Mill", icon: 'cafe'}] },
+  { slug: 'ten-oclock',    'shortlabel': '10',                 label: "Ten O'clock",                  waypoints: [] },
+  { slug: 'eleven-oclock', 'shortlabel': '11',                 label: "Eleven O'clock",               waypoints: [] },
+  { slug: 'twelve-oclock', 'shortlabel': '12 - Newick',        label: "Twelve O'clock - Newick",      waypoints: [
+    { lat: 50.923769, lng: 0.019516, name: "The Royal Oak, Newick", icon: 'pub' }
+
+
+
+  ]},
 ];
