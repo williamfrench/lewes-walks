c2e9ffdbdb7cc126af2b53f92b74743fd7e73cda
William French <idontactuallyneedthislol@gmail.com>
2026-03-17 17:51:57 +0000

    Move 7 oclock to 5 oclock

--- walks.js
+++ walks.js
@@ -3,11 +3,11 @@ const walks = [
   { slug: 'two-oclock',    'shortlabel': '2 - Halland',        label: "Two O'clock - Halland",        waypoints: [] },
   { slug: 'three-oclock',  'shortlabel': '3',                  label: "Three O'clock",                waypoints: [] },
   { slug: 'four-oclock',   'shortlabel': '4',                  label: "Four O'clock",                 waypoints: [] },
-  { slug: 'five-oclock',   'shortlabel': '5',                  label: "Five O'clock",                 waypoints: [] },
+  { slug: 'five-oclock',  'shortlabel': '5 - Bishopstone',    label: "Five O'clock - Bishopstone",  waypoints: [
+    { lat: 50.831916, lng: 0.034153, name: "YHA South Downs", icon: 'cafe'}]},
   { slug: 'six-oclock',    'shortlabel': '6 - Newhaven',       label: "Six O'clock - Newhaven",       waypoints:[
     { lat: 50.835848, lng: 0.012611, name: 'The Abergavenny Arms', icon: 'pub' }]},
-  { slug: 'seven-oclock',  'shortlabel': '7 - Bishopstone',    label: "Seven O'clock - Bishopstone",  waypoints: [
-    { lat: 50.831916, lng: 0.034153, name: "YHA South Downs", icon: 'cafe'}]},
+  { slug: 'seven-oclock',  'shortlabel': '7 - Bishopstone',    label: "Seven O'clock - Bishopstone",  waypoints: [] },
   { slug: 'eight-oclock',  'shortlabel': '8 - Brighton',       label: "Eight O'clock - Brighton",     waypoints: [] },
   { slug: 'nine-oclock',   'shortlabel': "9 - Devil's Dyke",   label: "Nine O'clock - Devil's Dyke",  waypoints: [
     { lat: 50.896557, lng: -0.164240, name: "The Plough Inn", icon:'pub'},
