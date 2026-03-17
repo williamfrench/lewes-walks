3053774657d6b2de259e2e53f4ca0c9bacb610fc
William French <idontactuallyneedthislol@gmail.com>
2026-03-17 12:06:09 +0000

    Add 6 oclock - Newhaven

--- walks.js
+++ walks.js
@@ -4,7 +4,8 @@ const walks = [
   { slug: 'three-oclock',  'shortlabel': '3',                  label: "Three O'clock",                waypoints: [] },
   { slug: 'four-oclock',   'shortlabel': '4',                  label: "Four O'clock",                 waypoints: [] },
   { slug: 'five-oclock',   'shortlabel': '5',                  label: "Five O'clock",                 waypoints: [] },
-  { slug: 'six-oclock',    'shortlabel': '6',                  label: "Six O'clock",                  waypoints: [] },
+  { slug: 'six-oclock',    'shortlabel': '6 - Newhaven',                  label: "Six O'clock - Newhaven",                  waypoints:[
+    { lat: 50.835848, lng: 0.012611, name: 'The Abergavenny Arms', icon: 'pub' }]},
   { slug: 'seven-oclock',  'shortlabel': '7',                  label: "Seven O'clock",                waypoints: [] },
   { slug: 'eight-oclock',  'shortlabel': '8 - Brighton',       label: "Eight O'clock - Brighton",     waypoints: [] },
   { slug: 'nine-oclock',   'shortlabel': "9 - Devil's Dyke",   label: "Nine O'clock - Devil's Dyke",  waypoints: [
@@ -15,6 +16,5 @@ const walks = [
     { lat: 50.910786, lng: -0.005657, name: "The Rainbow", icon: 'pub'},
     { lat: 50.892104, lng: -0.010713, name: "The Blacksmith's arms", icon: 'pub'}] },
   { slug: 'twelve-oclock', 'shortlabel': '12 - Newick',        label: "Twelve O'clock - Newick",      waypoints: [
-    { lat: 50.923769, lng: 0.019516, name: "The Royal Oak, Newick", icon: 'pub' }]
-  },
+    { lat: 50.923769, lng: 0.019516, name: "The Royal Oak, Newick", icon: 'pub' }]}
 ];
