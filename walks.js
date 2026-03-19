2e85f3419b9201d00a724d9492b41bab0998e692
William French <idontactuallyneedthislol@gmail.com>
2026-03-19 18:44:16 +0000

    Add 1 oclock - Isfield, 7 oclock Rottingdean and 10 oclock Hassocks. Tweak 8 oclock Brighton route

--- walks.js
+++ walks.js
@@ -1,5 +1,6 @@
 const walks = [
-  { slug: 'one-oclock',    'shortlabel': '1 - Isfield',        label: "One O'clock - Isfield",        waypoints: [] },
+  { slug: 'one-oclock',    'shortlabel': '1 - Isfield',        label: "One O'clock - Isfield",        waypoints: [
+   { lat:50.925663, lng:0.050629, name:"The Anchor Inn", icon: 'pub'}] },
   { slug: 'two-oclock',    'shortlabel': '2 - Halland',        label: "Two O'clock - Halland",        waypoints: [
     { lat: 50.896913, lng: 0.069888, name: "The Green Man", icon: 'pub'}] },
   { slug: 'three-oclock',  'shortlabel': '3 - Berwick',        label: "Three O'clock - Berwick",      waypoints: [
@@ -10,16 +11,22 @@ const walks = [
   { slug: 'four-oclock',   'shortlabel': '4 - Alfriston',      label: "Four O'clock - Alfriston",     waypoints: [
     { lat: 50.858830, lng: 0.070279, name: "The Trevor Arms", icon: "pub"},
     { lat: 50.859208, lng: 0.070378, name: "Steamworks", icon: 'pub'} ] },
-  { slug: 'five-oclock',   'shortlabel': '5 - Bishopstone',    label: "Five O'clock - Bishopstone",  waypoints: [
+  { slug: 'five-oclock',   'shortlabel': '5 - Bishopstone',    label: "Five O'clock - Bishopstone",   waypoints: [
     { lat: 50.831916, lng: 0.034153, name: "YHA South Downs", icon: 'cafe'}]},
   { slug: 'six-oclock',    'shortlabel': '6 - Newhaven',       label: "Six O'clock - Newhaven",       waypoints:[
     { lat: 50.835848, lng: 0.012611, name: 'The Abergavenny Arms', icon: 'pub' }]},
-  { slug: 'seven-oclock',  'shortlabel': '7',    label: "Seven O'clock",  waypoints: [] },
-  { slug: 'eight-oclock',  'shortlabel': '8 - Brighton',       label: "Eight O'clock - Brighton",     waypoints: [] },
+  { slug: 'seven-oclock',  'shortlabel': '7 - Rottingdean',    label: "Seven O'clock - Rottingdean",  waypoints: [
+    { lat: 50.857338, lng: -0.021527, name: "The Juggs", icon: 'pub'}] },
+  { slug: 'eight-oclock',  'shortlabel': '8 - Brighton',       label: "Eight O'clock - Brighton",     waypoints: [
+    { lat: 50.868483, lng: 0.000384, name: 'The Swan', icon: 'pub'} ] },
   { slug: 'nine-oclock',   'shortlabel': "9 - Devil's Dyke",   label: "Nine O'clock - Devil's Dyke",  waypoints: [
     { lat: 50.896557, lng: -0.164240, name: "The Plough Inn", icon:'pub'},
     { lat: 50.904657, lng: -0.146926, name: "The Coffee Mill", icon: 'cafe'}] },
-  { slug: 'ten-oclock',    'shortlabel': '10',                 label: "Ten O'clock",                  waypoints: [] },
+  { slug: 'ten-oclock',    'shortlabel': '10 - Hassocks',      label: "Ten O'clock - Hassocks",       waypoints: [
+    { lat: 50.921037, lng: -0.115845, name: "The White Horse", icon: 'pub'},
+    { lat: 50.921133, lng: -0.114650, name: "The Bull", icon: "pub"},
+    { lat: 50.902446, lng: -0.062249, name: "The Half Moon Inn", icon: 'pub'}
+    ] },
   { slug: 'eleven-oclock', 'shortlabel': '11 - Plumpton',      label: "Eleven O'clock - Plumpton",    waypoints: [
     { lat: 50.920773, lng: -0.048527, name: 'The Jolly Sportsman', icon: 'pub'},
     { lat: 50.910786, lng: -0.005657, name: "The Rainbow", icon: 'pub'},
